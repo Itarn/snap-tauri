9e6b391af541ae79606fa1938fd62c3063290a75
alan.wang <alan.wang@geekbang.com>
2023-05-22 08:22:44 +0800

    完成数据持久化验证

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,50 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+  all(not(debug_assertions), target_os = "macos"),
+  windows_subsystem = "none"
 )]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+use std::{collections::HashMap, sync::Mutex};
+use tauri::State;
+use serde_json::{json, to_string};
+
+// here we use Mutex to achieve interior mutability
+struct Storage {
+  store: Mutex<HashMap<String, String>>,
 }
+impl Storage {
+  fn get_entries_as_json(&self) -> Result<String, serde_json::Error> {
+    let lock = self.store.lock().unwrap();
+    let entries: Vec<_> = lock.iter().map(|(key, value)| json!({ "key": key, "value": value })).collect();
+    to_string(&entries)
+  }
+}
+
+#[tauri::command]
+fn load_storage (storage: State<Storage>) -> String  {
+  storage.get_entries_as_json().unwrap()
+}
+
+#[tauri::command]
+fn storage_insert(key: String, value: String, storage: State<Storage>) {
+  // mutate the storage behind the Mutex
+  storage.store.lock().unwrap().insert(key, value);
+}
+
+#[tauri::command]
+fn storage_delete(key: String, storage: State<Storage>) {
+  // mutate the storage behind the Mutex
+  storage.store.lock().unwrap().remove(&key);
+}
+
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//    format!("Hello, {}!", name)
+// }
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .manage(Storage { store: Default::default() })
+    .invoke_handler(tauri::generate_handler![load_storage, storage_insert, storage_delete])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { invoke } from '@tauri-apps/api/tauri'
 import { registerGloabalShortcutForSpecificApp } from './utils'
 import { open } from '@tauri-apps/api/dialog'
 // import { appWindow } from '@tauri-apps/api/window'
@@ -32,12 +33,45 @@ function openApplicationsDir () {
 function cancelShortcut () {
   unregisterAll()
 }
+
+function addStr () {
+  invoke('storage_insert', { key: '1', value: 'xxx' })
+  invoke('storage_insert', { key: '2', value: 'fff' })
+  invoke('storage_insert', { key: '3', value: 'xdddxx' })
+  invoke('storage_insert', { key: '4', value: 'xxgggx' })
+}
+function getStr () {
+  invoke('load_storage').then(res => {
+    console.log(res)
+  })
+}
+function deleteStr () {
+  invoke('storage_delete', { key: '1' })
+}
+function modifyStr () {
+  invoke('storage_insert', { key: '2', value: 'yyy' })
+}
+
+// 在前端代码中
+let pressedKeys = new Set();
+
+window.addEventListener('keydown', (event) => {
+  pressedKeys.add(event.key);
+  console.log(event)
+})
+window.addEventListener('keyup', (event) => {
+  console.log(pressedKeys)
+});
 </script>
 
 <template>
   <div class="container">
     <button @click="openApplicationsDir">注册快捷键</button>
     <button @click="cancelShortcut">取消快捷键</button>
+    <button @click="addStr">添加数据</button>
+    <button @click="deleteStr">删除数据</button>
+    <button @click="modifyStr">修改数据</button>
+    <button @click="getStr">获取已经添加过的数据</button>
   </div>
 </template>
 
